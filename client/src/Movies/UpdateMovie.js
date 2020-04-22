0ea1a601dfcec0d650beeb260e5b8dc9a416b210
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-04-22 15:52:39 -0400

    Created basic forms for the updatemovie component

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -3,7 +3,28 @@ import React from "react";
 const UpdateMovie = () => {
     return (
         <div>
-
+            <h2>
+                <form>
+                    <input 
+                        type="text"
+                        name="title"
+                        placeholder="Movie Title"
+                        value=""
+                    /><br/>
+                    <input 
+                        type="text"
+                        name="director"
+                        placeholder="Director"
+                        value=""
+                    /><br/>
+                    <input 
+                        type="number"
+                        name="metascore"
+                        placeholder="Metascore"
+                        value=""
+                    /><br/>
+                </form>
+            </h2>
         </div>
     )
 }
